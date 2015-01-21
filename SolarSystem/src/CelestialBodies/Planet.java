67ab23cf67c75e2193e7ef21df2caeaa921e599c
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-21 14:07:35 -0800

    Reorganized Classes

---
+++ SolarSystem/src/CelestialBodies/Planet.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CelestialBodies;
+
+/**
+ *
+ * @author jnuez_000
+ */
+public class Planet {
+    private double mass;
+    private double radius;
+    
+    public void setMass(double m)
+    {
+        mass = m;
+    }
+    
+    public void setRadius(double r)
+    {
+        radius = r;
+    }
+    
+    public double getMass()
+    {
+        return mass;
+    }
+    
+    public double getRadius()
+    {
+        return radius;
+    }
+}
